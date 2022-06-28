e0ad0aa563477d3d68a7fa0d98c45a42c5f2dc23
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-06-28 19:03:38 -0300

    yyy

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-var rho = new OmegaNum(0)
+var theta = new OmegaNum(0)
 
 var getTheta = (level) => new OmegaNum(level * 2)
+var getDelta = (level) => new OmegaNum(level * 5)
 
 theta = new Theta();
+delta = new Delta();
 
 function Theta() {
     this.level = 0;
     this.price = new OmegaNum(25)
+}
+
+function Delta() {
+    this.level = 0;
+    this.price = new OmegaNum(600)
 }
 
 document.getElementById("cost1").onclick = () => {
@@ -18,7 +25,17 @@ document.getElementById("cost1").onclick = () => {
     }
 }
 
+document.getElementById("cost2").onclick = () => {
+    if (rho.gte(delta.price) == true) {
+        rho = rho.sub(delta.price)
+        delta.price = delta.price.mul(1.3)
+        delta.level += 1
+        document.getElementById("cost2").innerHTML = "Cost:" + theta.price + "(&delta; = " + getTheta(theta.level) + ")"
+    }
+}
+
+
 setInterval(() => {
-    rho = rho.add(new OmegaNum(1).times(getTheta(theta.level).add(1)))
+    rho = rho.add(new OmegaNum(1).times(getTheta(theta.level).times(getDelta(delta.level).add(1)).add(1)))
     document.getElementById("rho").innerHTML = "You Have " + rho + " Rho"
 }, 100);
