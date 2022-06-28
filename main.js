10b227b7222126af2d118d0e4772d64cee4a2c53
Annontations6 <106895624+Annontations6@users.noreply.github.com>
2022-06-28 18:28:35 -0300

    yes

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 var rho = new OmegaNum(0)
+
+var getTheta = (level) = new OmegaNum(level * 2)
+
+theta = new Theta();
+
+function Theta() {
+    this.level = 0;
+    this.price = new OmegaNum(25)
+}
+
+setInterval(() => {
+    rho = rho.times(getTheta(theta.level)).add(1)
+}, 100);
